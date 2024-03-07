8a69191537dc5841f9d6bbe6c9bd4d4ae9d4a211
Siddhant <siddhantvsangole@gmail.com>
2024-03-07 21:40:34 +0530

    lamp

---
+++ script.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector("#btn");
+let light = document.querySelector("#light");
+
+function toggleBtn(){
+    btn.classList.toggle("active");
+    light.classList.toggle("on");
+}
